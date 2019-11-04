6dccdd0b126d559cc62801556c3bbbff5a685068
Rajesh Kumar Dwivedi <rajeshdwi@gmail.com>
2019-11-03 21:57:58 -0800

    initial commit

---
+++ src/demo/FinalJson.java
@@ -0,0 +1,20 @@
+package demo;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * @author Rajesh Dwivedi
+ * Date: 11/1/19
+ */
+public class FinalJson {
+    private InitialJson listOfFruits;
+
+    @JsonProperty("List Fruits")
+    public InitialJson getListOfFruits() {
+        return listOfFruits;
+    }
+
+    public void setListOfFruits(InitialJson listOfFruits) {
+        this.listOfFruits = listOfFruits;
+    }
+}
